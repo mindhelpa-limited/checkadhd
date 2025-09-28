1534003052a55462c0017760863140ef0a8a1f93
courageoususahealth-rgb <courageoususahealth@gmail.com>
2025-09-28 16:17:38 +0100

    Fix checkout session code with logs

--- src/app/how-it-works/page.jsx
+++ src/app/how-it-works/page.jsx
@@ -192,7 +192,7 @@ export default function HowItWorksPage() {
             href="/services"
             className="mt-8 inline-block bg-blue-600 text-white font-bold py-4 px-8 rounded-full shadow-lg transition-transform transform hover:scale-105 glow-on-hover"
           >
-            View Our Services & Get Started
+            Get Started
           </a>
         </div>
         <div className="absolute bottom-0 left-0 w-full h-px">
