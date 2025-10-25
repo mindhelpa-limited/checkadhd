11f918959ba20574c0f2bd0ca437b4a4f7267b92
courageoususahealth-rgb <courageoususahealth@gmail.com>
2025-10-25 10:45:57 +0100

    Your commit message

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ✅ Disable ESLint errors on Vercel build
   },
+  experimental: {
+    appDir: true, // ✅ Enable Next.js App Router inside src/app
+  },
 };
 
 module.exports = nextConfig;
